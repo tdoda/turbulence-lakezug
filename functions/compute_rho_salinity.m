66a979653e443d27672c59d7f39e36e0c9063be6
Tomy Doda <tomy.doda@gmail.com>
2025-01-09 18:18:40 +0100

    Add density and salinity calculation for Lake Zug (preliminary)

--- functions/compute_rho_salinity.m
+++ functions/compute_rho_salinity.m
@@ -15,7 +15,7 @@ function [rhoTS,Cmatch,Sal,depth] = compute_rho_salinity(lakename,Temp,Cond,Pres
 %   rhoTS (1*n double array): density [kg m^-3] as a function of Temperature and Salinity
 %   Cmatch (1*n double array): corrected conductivity [uS/cm] to match
 %   temperature data
-%   Salinity (1*n double array): Salinity [g kg^-1]
+%   Sal (1*n double array): Salinity [g kg^-1]
 %   depth (1*n double array): depth [m] (not corrected for atmospheric
 %   pressure)
 %
@@ -37,7 +37,24 @@ elseif strcmp(lakename,'Geneva')
     BetaS=0.807e-3; % Haline contraction coefficient [kg/g]
     gS=0.874e-3; % Coefficient converting conductivity at 20°C to salinity [g cm kg^-1 uS^-1]
     g=9.81; % gravitational acceleration, [m s^-2]
-
+elseif strcmp(lakename,'Zug')
+    % *********** NEED TO CHECK AND UPDATE THE ESTIMATES **************
+    gS=0.79e-3; %  Coefficient converting conductivity at 20°C to salinity [g cm kg^-1 uS^-1]  (Boisgontier, 2016)
+    
+    rho0 = 999.84298 + 1e-3*(65.4891*Temp - 8.56272*Temp.^2 + 0.059385*Temp.^3); %doesn't work for temperature higher than 25°C
+    if JAC_correction
+        [~,Cmatch] = salinity_JAC(Press,Temp,Cond);
+        Cmatch=Cmatch*1000; % [uS/cm]
+    else
+        Cmatch=Cond;
+    end
+    % Convert conductivity (C) to C_20°C
+    [Cond20,~,~]=conductivity_20(Temp,Cmatch); % [uS/cm]
+    Sal=gS*Cond20; % [g/kg]
+    rhoTS=rho0.*(1+gS*1e-3*Cond20); % [kg/m3]
+    mrho = cumsum(rhoTS)./(1:length(rhoTS))';
+    depth=10000*Press./(mrho*9.81);
+    return
 else
     error('Wrong lake name!')
 end
@@ -71,6 +88,8 @@ depth=10000*Press./(mrho*g);
 end
 
 function [Cond20,alpha,dCdT]=conductivity_20(Temp,Cond)
+% CAUTION: could vary with ionic composition and should ideally be updated
+% rom one lake to another (T. Doda)
     %
     %******************************INFO ***********************************
     %
