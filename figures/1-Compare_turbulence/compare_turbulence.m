b9a4cd568afdc804af465a14048e889d52937d6a
Tomy Doda <tomy.doda@gmail.com>
2025-02-12 11:07:38 +0100

    New figures about turbulence profiles and spectra from last campaigns

--- figures/1-Compare_turbulence/compare_turbulence.m
+++ figures/1-Compare_turbulence/compare_turbulence.m
@@ -7,10 +7,11 @@ clc
 %% Parameters
 
 savefig=false;
-campaign_name='20241205';
+% campaign_name='20211110';
+campaign_name='20250211';
 datafolder=['..\..\data\VMP\',campaign_name,'\Level2\'];
 folder_param_name='down1.0_K5.3_L_NAS0.0058_singlepole_nfft3_EPFL_Goodman';
-filenames={'VMP010','VMP011'};
+filenames={'VMP004','VMP007','VMP008'};
 
 addpath("..\..\functions\figures\") % Add functions
 
@@ -22,16 +23,23 @@ for kf=1:length(filenames)
     datavar=load([datafolder,filenames{kf},'_',folder_param_name,'\results_',filenames{kf},'_down.mat']);
     datavar.filename=filenames{kf};
     DATA(kf)=datavar;
+
 end
 
 
 %% Figures turbulence
 col_day1=winter(length(filenames));
 %col_day1(3,:)=[0,0,0];
-[fig1,fig2]=plot_comparison_turbulence(DATA,filenames,'longest',col_day1);
+[fig1,fig2,fig3,fig4]=plot_comparison_turbulence(DATA,filenames,'longest',col_day1);
 
 if savefig
-    saveas(fig1,['turbulence_profiles',filenames{:},'.fig'])
-    exportgraphics(fig1,['turbulence_profiles',filenames{:},'.png'],'Resolution',400)
+    saveas(fig1,['CTD_profiles_',campaign_name,'_',filenames{:},'.fig'])
+    exportgraphics(fig1,['CTD_profiles_',campaign_name,'_',filenames{:},'.png'],'Resolution',400)
+
+    saveas(fig2,['fast_profiles_',campaign_name,'_',filenames{:},'.fig'])
+    exportgraphics(fig2,['fast_profiles_',campaign_name,'_',filenames{:},'.png'],'Resolution',400)
+
+    saveas(fig3,['turbulence_profiles_',campaign_name,'_',filenames{:},'.fig'])
+    exportgraphics(fig3,['turbulence_profiles_',campaign_name,'_',filenames{:},'.png'],'Resolution',400)
 end
 
--- figures/2-Spectra_bin/spectra_bin.m
+++ figures/2-Spectra_bin/spectra_bin.m
@@ -6,12 +6,15 @@ clc
 
 %% Parameters
 
-savefig=false;
-campaign_name='20241205';
+savefig=true;
+campaign_name='20250211';
 datafolder=['..\..\data\VMP\',campaign_name,'\Level2\'];
+% folder_param_name='down2.0_K5.3_L_NAS0.0058_singlepole_nfft3_EPFL_Goodman';
+% folder_param_name='down2.0_K5.3_L_NAS0.0058_singlepole_nfft5_EPFL_Goodman';
 folder_param_name='down1.0_K5.3_L_NAS0.0058_singlepole_nfft3_EPFL_Goodman';
-profname='VMP001';
-depth_bin=[50]; % [m]
+% folder_param_name='down0.5_K5.3_L_NAS0.0058_singlepole_nfft5_EPFL_Goodman';
+profname='VMP003';
+depth_bin=[119]; % [m]
 
 addpath("..\..\functions\figures\") % Add functions
 
@@ -34,11 +37,18 @@ for kbin=1:length(depth_bin)
 
     [~,ind_bin]=min(abs(data_prof_bin.depth-depth_bin(kbin)));
    
-    fig1=plot_spectrum_FP07(data_prof_bin,DATA.FAST{indprof},ind_bin);
+    fig1=plot_spectrum_FP07(data_prof_bin,DATA.BINNED{indprof}.SPECTRUMT1,DATA.param,ind_bin,'T1');
+    fig2=plot_spectrum_FP07(data_prof_bin,DATA.BINNED{indprof}.SPECTRUMT2,DATA.param,ind_bin,'T2');
+
+    if savefig
+        saveas(fig1,['spectrum_',campaign_name,'_',folder_param_name,'_',profname,'_',num2str(ind_bin),'_T1.fig'])
+        exportgraphics(fig1,['spectrum_',campaign_name,'_',folder_param_name,'_',profname,'_',num2str(ind_bin),'_T1.png'],'Resolution',400)
+
+        saveas(fig2,['spectrum_',campaign_name,'_',folder_param_name,'_',profname,'_',num2str(ind_bin),'_T2.fig'])
+        exportgraphics(fig2,['spectrum_',campaign_name,'_',folder_param_name,'_',profname,'_',num2str(ind_bin),'_T2.png'],'Resolution',400)
+        disp('Figures saved!')
+    end
 end
 
-% if savefig
-%     saveas(fig1,['turbulence_profiles',filenames{:},'.fig'])
-%     exportgraphics(fig1,['turbulence_profiles',filenames{:},'.png'],'Resolution',400)
-% end
+
 
